9939f6ac644dd692ddcfbd16842d38240eef2ba7
830503 <lixiaoqi19830503@gmail.com>
2022-01-11 17:00:08 +0100

    refactoring by using session bag, but  unknown name does not work

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
-
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\VarDumper\Cloner\AbstractCloner;
 
 class LearningController extends AbstractController
 {
+    private $session;
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
     #[Route('/learning', name: 'learning')]
     public function index(): Response
     {
@@ -21,30 +27,39 @@ class LearningController extends AbstractController
         ]);
     }
 
-    #[Route('/aboutme', name: 'aboutme')]
-    public function AboutMe(): Response
-    {
-        return $this->render('aboutme/index.html.twig', ['name' => 'About me']);
-    }
-
     #[Route('/', name: 'homepage')]
     public function ShowMyName(): Response
     {
-        if ($_POST) {
-            $name = $_POST['name'];
+        // $name = 'Unknown';
+        if (!empty($this->session->get('name'))) {
+            $name = $this->session->get('name');
         } else {
-            $name = "Unknown";
+            $name = "unknown";
         }
         return $this->render('homepage/index.html.twig', ['name' => $name]);
     }
 
-    #[Route('/changeMyName', name: 'change')]
-    public function ChangeMyName()
+    #[Route('/changeMyName', name: 'changeMyName', methods: ['POST'])]
+    public function ChangeMyName(): RedirectResponse
     {
         if (!empty($_POST['name'])) {
-            $_SESSION['name'] = $_POST['name'];
+            $name = $_POST['name'];
+            $this->session->set('name', $name);
+        } else {
+            $name = "unknown";
         }
-        return $this->render('homepage/index.html.twig', ['name' => $_SESSION['name']]);
-        // return $this->redirectToRoute('homepage');
+        return $this->redirectToRoute('homepage');
+    }
+
+    #[Route('/aboutme', name: 'aboutme')]
+    public function AboutMe(): Response
+    {
+        // $name = "Unknown";
+        if (!empty($this->session->get('name'))) {
+            $name = $this->session->get('name');
+            return $this->render('aboutme/index.html.twig', ['name' => $name]);
+        } else {
+            return $this->forward('App\Controller\LearningController::ShowMyName');
+        }
     }
 }
